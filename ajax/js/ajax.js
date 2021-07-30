756b3addf208b0b2fed5f17d8e760df638525399
Michelle Jowers <michelle.jowers@icloud.com>
2021-07-30 12:06:55 -0400

    update files

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -2,4 +2,29 @@
 
 $(() => {
     console.log("Everything is ready!");
+
+    $.getJSON(`http://localhost:8080/api/users/`)
+        .done(res => {
+            console.debug(res);
+            display(res);
+        })
+        .fail(err => {
+            console.error(err);
+        });
 }); 
+
+const display = (users) => {
+    let tbody = $("tbody");
+    tbody.empty();
+    for(let user of users) {
+        let tr = $("<tr></tr>");
+        tr.append( $(`<td>${user.id}</td>`) );
+        tr.append( $(`<td>${user.username}</td>`) );
+        tr.append( $(`<td>${user.firstName} ${user.lastName}</td>`) );
+        tr.append( $(`<td>${user.phone}</td>`) );
+        tr.append( $(`<td>${user.email}</td>`) );
+        tr.append( $(`<td>${user.reviewer ? "Yes" : "No"}</td>`) );
+        tr.append( $(`<td>${user.admin ? "Yes" : "No"}</td>`) );
+        tbody.append(tr);
+    }
+};
