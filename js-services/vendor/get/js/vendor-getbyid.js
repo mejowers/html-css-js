618b8ff448ec5130a8a35a180b6ff639dc9cac01
Michelle Jowers <michelle.jowers@icloud.com>
2021-08-02 14:38:24 -0400

    add and update files

---
+++ js-services/vendor/get/js/vendor-getbyid.js
@@ -0,0 +1,25 @@
+
+$(() => {
+
+    $("#zGet").on("click", () => {
+        let id = $("#zId").val();
+        vendorGetById(id)
+            .done(res => {
+                console.debug(res);
+                display(res);
+            })
+            .fail(err => console.error(err));
+    })
+});
+
+const display = (vendor) => {
+    $("#dId").val(vendor.id);
+    $("#dCode").val(vendor.code);
+    $("#dName").val(vendor.name);
+    $("#dAddress").val(vendor.address);
+    $("#dCity").val(vendor.city);
+    $("#dState").val(vendor.state);
+    $("#dZip").val(vendor.zip);
+    $("#dPhone").val(vendor.phone);
+    $("#dEmail").val(vendor.email);    
+}
